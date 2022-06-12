e396718d9c09ce42ace96aef99f7376ffca94563
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-06-12 11:59:26 -0300

    Creating a modal to read json data and organizing by folders

---
+++ Model/Landmark.swift
@@ -0,0 +1,32 @@
+import Foundation
+import SwiftUI
+import CoreLocation
+
+//We use codable because its easy to transfer data betwen the strcture and datafile
+struct Landmark: Codable, Hashable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    //Declare images: use private because the user only want to see itself image
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+        }
+    
+    //Add cordinates property to the structure using nested coordinates:
+        private var coordinates: Coordinates
+    
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, longitude: coordinates.longitude
+        )
+    }
+        
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+    }
+}
