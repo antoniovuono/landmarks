d5c1deaa6f4464115717745c2e3c4d1fafdff8c9
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-06-11 23:41:21 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Antonio Vuono on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
